ff5f00d5cbbd03b05d3816dcfde38634b8fea274
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-11-22 17:08:46 +0100

    add: text added

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<template lang="">
+  <!-- <h1>App.Vue</h1> -->
 
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <!-- utilizzo il componente -->
+  <AppTitle />
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+
+// importa il componente
+import AppTitle from './components/AppTitle.vue';
+
+export default {
+  components: {
+    // registra il componente
+    AppTitle
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+</script>
+
+<style lang="">
+  
 </style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,5 @@
+
+<template lang="">
+    <h1>La mia prima app con Vite</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae, laudantium eligendi repellendus consequuntur culpa, dicta beatae officia eius neque sed illum numquam. Corrupti ducimus quae corporis a animi quo debitis.</p>
+</template>
